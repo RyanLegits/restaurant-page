cf7b0a476a58d6d2ef3f2d18a4f20362ade79456
RyanLegits <ryan.legits@gmail.com>
2022-09-18 17:28:17 -0600

    Add functions to create background, header, hero and info containers; Add some content to background, header and hero containers including logo, navbar, title and subtitle

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,11 +1,89 @@
 // Append html elements to DOM (including: header, hero and info)
 
-export default function pageLoad() {
-
+// Create background to span header and hero
+function createBackground() {
 	const content = document.querySelector('#content');
+
+	// Create background container
+	const backgroundCont = document.createElement('div');
+	backgroundCont.classList.add('background-cont');
+	content.appendChild(backgroundCont);
+
+}
+
+function createHeader() {
+	const backgroundCont = document.querySelector('.background-cont');
+
 	// Create header
 	const header = document.createElement('div');
 	header.classList.add('header');
-	header.textContent = "Test 1 2";
-	content.appendChild(header);
+	backgroundCont.appendChild(header);
+
+	// Create header logo container
+    const headerLogoCont = document.createElement('div');
+    headerLogoCont.classList.add('header-logo-cont');
+    header.appendChild(headerLogoCont);
+
+    // Create header logo
+    const headerLogo = document.createElement('h3');
+    headerLogo.classList.add('header-logo');
+    headerLogo.innerHTML = "Header Logo";
+    headerLogoCont.appendChild(headerLogo);
+
+    // Create header navigation container
+    const headerNavCont = document.createElement('div');
+    headerNavCont.classList.add('header-nav-cont');
+    header.appendChild(headerNavCont);
+
+    // Create nav menu
+    const navMenu = document.createElement('ul');
+    navMenu.classList.add('nav-menu');
+    headerNavCont.appendChild(navMenu);
+
+    // Create nav menu items
+    
+    // To add a menu item to page, add it to the list here
+    const menuItems = ['one', 'two', 'three'];
+
+    for (let i = 0; i < menuItems.length; i++) {
+    	const item = menuItems[i];
+    	const navItem = document.createElement('li');
+    	navItem.appendChild(document.createTextNode(item));
+    	navMenu.appendChild(navItem);
+
+    }
+}
+
+function createHero() {
+	const backgroundCont = document.querySelector('.background-cont');
+
+	// Create hero
+	const hero = document.createElement('div');
+	hero.classList.add('hero');
+	backgroundCont.appendChild(hero);
+
+	// Create hero text container
+	const heroTextCont = document.createElement('div');
+    heroTextCont.classList.add('hero-text-cont');
+    hero.appendChild(heroTextCont);
+
+    // Create hero title
+    const heroTitle = document.createElement('h1');
+    heroTitle.appendChild(document.createTextNode('Ramen Restaurant'));
+    heroTextCont.appendChild(heroTitle);
+
+    // Create hero sub title
+    const heroSub = document.createElement('h6');
+    heroSub.appendChild(document.createTextNode('Text'));
+    heroTextCont.appendChild(heroSub);
+}
+
+function createInfo() {
+	
+}
+
+export default function pageLoad() {
+	createBackground();
+	createHeader();
+	createHero();
 }
