f2a8d3f12fec0754affb041b8cfe43f849a0608e
RyanLegits <ryan.legits@gmail.com>
2022-10-09 19:39:07 -0600

    Add content to ‘Contact’ page

--- src/contact.js
+++ src/contact.js
@@ -14,6 +14,39 @@ function createContact() {
     elFactory('h1', {}, 'Contact')
   );
   contact.appendChild(contactTitleCont);
+
+  const contactCont = elFactory('div', { class: 'contact-cont' });
+  contact.appendChild(contactCont);
+
+  // Create contact section container
+  const contactSecCont = elFactory(
+    'div',
+    { class: 'contact-sec-cont' },
+    elFactory('h6', {}, 'Call Us'),
+    elFactory('p', {}, '(555) 642-7878'),
+    elFactory('p', {}, '1 (800) 555-7289'),
+    elFactory('h6', {}, 'Location'),
+    elFactory('p', {}, '4724 Main Street,'),
+    elFactory('p', {}, 'Seattle, WA 98104'),
+    elFactory('h6', {}, 'Hours'),
+    elFactory(
+      'div',
+      { class: 'sec-hours' },
+      elFactory(
+        'div',
+        { class: 'hours' },
+        elFactory('p', {}, 'Monday-Friday'),
+        elFactory('p', {}, '8AM-5PM')
+      ),
+      elFactory(
+        'div',
+        { class: 'hours' },
+        elFactory('p', {}, 'Saturday-Sunday'),
+        elFactory('p', {}, '9AM-3PM')
+      )
+    )
+  );
+  contactCont.appendChild(contactSecCont);
 }
 
 export default function loadContact() {
